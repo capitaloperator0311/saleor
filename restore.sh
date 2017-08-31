1ce8802f79bf451550416e08eab48e337196dbe5
ricardojikal0311 <15992876+ricardojikal0311@users.noreply.github.com>
2017-08-31 05:54:36 +0000

    Add new field on AssignedVariantAttributeValue

--- restore.sh
+++ restore.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2017-07-01"  # YYYY-MM-DD
-END_DATE="2017-08-28"    # YYYY-MM-DD
+START_DATE="2017-08-01"  # YYYY-MM-DD
+END_DATE="2017-11-28"    # YYYY-MM-DD
 
 # Min and max commits per selected day
-MIN_COMMITS=2
+MIN_COMMITS=1
 MAX_COMMITS=4
 
 # Min and max days per month to commit
