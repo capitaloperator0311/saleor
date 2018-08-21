6c02ae242764fee48591659d904636aeecdf1c7a
ricardojikal0311 <15992876+ricardojikal0311@users.noreply.github.com>
2018-08-21 17:19:15 +0000

    Fix negative unit price on gift line

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2017-08-01"  # YYYY-MM-DD
-END_DATE="2017-11-28"    # YYYY-MM-DD
+START_DATE="2018-08-01"  # YYYY-MM-DD
+END_DATE="2018-11-28"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=1
