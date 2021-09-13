0947e62a1070f0aa9b86087c43bb9b7922298680
ricardojikal0311 <15992876+ricardojikal0311@users.noreply.github.com>
2021-09-13 09:35:44 +0000

    Adjust app extension filters to new fields

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2020-08-01"  # YYYY-MM-DD
-END_DATE="2020-11-28"    # YYYY-MM-DD
+START_DATE="2021-09-01"  # YYYY-MM-DD
+END_DATE="2021-12-28"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=1
